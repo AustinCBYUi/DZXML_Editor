5ac73cc5f6dd570f1280564f6d737ea1f314abb8
infraredzs <111200205+infraredzs@users.noreply.github.com>
2023-04-29 14:25:11 -0700

    Deleted xml_auto_editor.py file and merged all to dzxml.py file.

--- DZXML.py
+++ DZXML.py
@@ -1,4 +1,6 @@
-import xml_auto_editor as xml
+# import xml_auto_editor as xml
+import xml.etree.ElementTree as ET
+from pathlib import *
 
 #Author - Austin Campbell || Released on my school github acc.
 #Purpose - Automate types.xml editing without screwing up loot tables / exponential nominal / min values.
@@ -10,12 +12,152 @@ import xml_auto_editor as xml
 #Email - infrared.dayz@gmail.com
 #Discord - Danny2#5070
 
-menu_stop = False
 
+file_to_edit = PurePath("types.xml")
+
+#Primary tree and root to be edited.
+tree = ET.parse(file_to_edit)
+root = tree.getroot()
+
+#Constant for functionality / feedback after performing actions
+MSG = "Modified!"
+
+
+#-------- Nominal Modifiers ---------#
+
+#Prompt for multiplying or adding to the nominal values.
+def nominal_selection():
+    multiply_or_add = int(input("Multiply or Add nominal?(0 - Multiply, 1 - Add): "))
+
+    if multiply_or_add == 0:
+        change_value = int(input("How much would you like to multiply each nominal value by? Use integers: "))
+        #Parameter is the change_value variable we have above
+        nominal_multiplication_modifier(change_value)
+    elif multiply_or_add == 1:
+        change_value = int(input("How much to add to the nominal? Use integers: "))
+        #Parameter is the change_value variable we have above
+        nominal_addition_modifier(change_value)
+
+
+#Nominal modifiers
+def nominal_multiplication_modifier(change_value):
+    for nominal in root.iter("nominal"):
+        #Multiply whatever user input is in the input
+        nominal.text = str(int(nominal.text) * change_value)
+    print(MSG)
+
+
+#Nominal modifiers
+#Changing nominal values by adding user value to pre-existing value
+def nominal_addition_modifier(change_value):
+    #For loop to loop through the <nominal></nominal> tag
+    for nominal in root.iter("nominal"):
+        #Add whatever integer the user puts in to the pre-existing value.
+        nominal.text = str(int(nominal.text) + change_value)
+    print(MSG)
+
+
+#-------- Minimum Modifiers --------#
+
+#function to modify min values
+def minimum_selection():
+    multiply_or_add = int(input("Multiply or Add Min?(0 - Multiply, 1 - Add): "))
+    if multiply_or_add == 0:
+        change_value = int(input("How much to multiply each min value by? Use integers:"))
+        #minimum_multiplication_modifier(change_value)
+    elif multiply_or_add == 1:
+        change_value = int(input("How much to add to each min value? Use integers: "))
+        minimum_addition_modifier(change_value)
+
+
+#Modifier to add to mins
+#If min value is 2 and you add 4 the new min will be 6.
+def minimum_addition_modifier(change_value):
+    #Loop through all min elements
+    for min in root.iter("min"):
+        #access values in min and add user input value
+        min.text = str(int(min.text) + change_value)
+    print(MSG)
+
+
+#Modifier to multiply from min
+#If min value is 2 and you multiply by 2 the new min will be 4.
+def minimum_multiplication_modifier(change_value):
+    #loop through all min elements
+    for min in root.iter("min"):
+        #access values in min and multiply by user input value
+        #This here already converts the stuff to a string outside, but inside is still treated like a integer!
+        min.text = str(int(min.text) * change_value)
+    print(MSG)
+
+
+#-------- Quantmin Modifier --------#
+#Commentating my issues with this function:
+#Python is always reading the xml as a string, so always convert our inputs or values to a str() first.
+def quantmin_modifier():
+    print("Must use integer and entire value, so if you want 80% type 80.")
+    change_value = int(input("What would you like to set the quantmin to?: "))
+    for quantmin in root.iter("quantmin"):
+        quantmin.text = str(int(quantmin.text))
+        if quantmin.text != "-1": #The -1 in the xml is a int, but Python is reading as a string, so had to change this to "-1"
+            #Set the value to whatever the user typed
+            quantmin.text = str(change_value) #Had to add str in front to convert the user input look above for reason
+            #If quantmin is already "-1", keep it at "-1"
+        elif quantmin.text == "-1":
+            quantmin.text = "-1"
+    print(MSG)
+
+
+#-------- Quantmax Modifier ---------#
+def quantmax_modifier():
+    #Little bit of direction for users
+    print("Must use integer and entire value, so if you want 100% type 100.")
+    #Get input
+    change_value = int(input("What would you like to set quantmax to?: "))
+    #run loop
+    for quantmax in root.iter("quantmax"):
+        quantmax.text = str(int(quantmax.text))
+        if quantmax.text != "-1":
+            #Set the value to whatever the user typed
+            quantmax.text = str(change_value)
+        elif quantmax.text == "-1":
+            quantmax.text = "-1"
+    print(MSG)
+
+
+
+#Need to get this working.
+# def minimum_check():
+#     minimum_text = str(int(minimum.text))
+#     maximum_text = str(int(maximum.text))
+#     nominal_root = root.iter("nominal") #Grabs current nominal root
+#     nominal_value = str(int(nominal_value.text)) #Current value
+#     minimum_root = root.iter("min") #Grabs current min value
+#     minimum_value = str(int(minimum_value.text))
+#     for min in root.iter("min"):
+#         if minimum_value > nominal_value:
+
+#     print(nominal_value)
+
+#TODO List:
+#  1 - Clean up user dialogue.
+# 2 - Make a check work for min - nominal
+# 3 - Maybe add a lifetime modifier
+# 4 - Try to get an icon for the .exe so it doesn't look so sketch
+# 5 - Maybe make the multiplier able to multiply only up to 5x including decimal points but rounds to a whole number for the types.xml
+# 6 - Build as .exe and include a blank types.xml in a zip with the github code as well.
+#7 - Write documentation including docstrings for the functions just in-case?
+
+#TODO Due Date:
+#Must complete the above todo list by Sunday 04/30/23.
+ 
+menu_stop = False
 #TODO Finish writing options menu for specific selections.
 while menu_stop == False:
     #Used a variable instead of a print statement to help cleanliness
     options_menu = """
+    DZXML Types Modifier
+    Please read the documentation before using. Thank you!
     1) Modify Nominal Values
     2) Modify Minimum Values
     3) Modify Quantity Minimum Values
@@ -29,12 +171,16 @@ while menu_stop == False:
 
     #Option 1 is modifying the nominal values
     if user_selected == 1:
-        xml.nominal_selection()
+        nominal_selection()
     #Option 2 is modifying the min values
     elif user_selected == 2:
-        xml.minimum_selection()
+        minimum_selection()
     elif user_selected == 3:
-        xml.quantmin_modifier()
+        quantmin_modifier()
+    elif user_selected == 4:
+        quantmax_modifier()
+        #TODO Get the program to write file to same path as exe
     elif user_selected == 0:
-        xml.tree.write("types_modified.xml")
+        tree.write("types_modified.xml")
+        menu_stop = True
         exit()
