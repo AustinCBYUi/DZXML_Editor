1618183c25c68cd1b1077bc49826ffafa5e989ec
infraredzs <111200205+infraredzs@users.noreply.github.com>
2023-04-29 22:05:24 -0700

    Added docstrings and minor modifications to file.

--- DZXML.py
+++ DZXML.py
@@ -13,47 +13,63 @@ from pathlib import *
 #Discord - Danny2#5070
 
 
-file_to_edit = PurePath("types.xml")
+# file_to_edit = PurePath("types.xml")
+file_to_edit = Path(r"Projects\DZXML_Editor\types.xml") #Dev
 
 #Primary tree and root to be edited.
 tree = ET.parse(file_to_edit)
 root = tree.getroot()
 
 #Constant for functionality / feedback after performing actions
-MSG = "Modified!"
+MSG = "\n\033[1;32;48m** Modified! **" #Will be Green
 
 
 #-------- Nominal Modifiers ---------#
 
 #Prompt for multiplying or adding to the nominal values.
 def nominal_selection():
-    multiply_or_add = int(input("Multiply or Add nominal?(0 - Multiply, 1 - Add): "))
+    """Selection menu for nominal editing."""
+    multiply_or_add = int(input("\033[1;35;48m Multiply or Add off nominal?(0 = Multiply, 1 = Add):\n"))
 
     if multiply_or_add == 0:
-        change_value = int(input("How much would you like to multiply each nominal value by? Use integers: "))
+        change_value = float(input("\033[0;36;48mHow much would you like to multiply each nominal value by? Use integers:\n"))
+        #Check if you are trying to multiply too high..
+        if change_value >= 6:
+            print("\033[0;31;48m Integer is too large. Please try a smaller number.")
+            #If you are, return back to the beginning.
+            nominal_selection()
         #Parameter is the change_value variable we have above
         nominal_multiplication_modifier(change_value)
     elif multiply_or_add == 1:
-        change_value = int(input("How much to add to the nominal? Use integers: "))
+        change_value = float(input("\033[0;36;48mHow much to add to the nominal? Use integers:\n")) #Converted to float
         #Parameter is the change_value variable we have above
         nominal_addition_modifier(change_value)
 
 
 #Nominal modifiers
 def nominal_multiplication_modifier(change_value):
+    """Multiplication modifier for nominal values, takes parameter change_value as a user input value and loops through all
+    parts in the root xml to change values."""
     for nominal in root.iter("nominal"):
         #Multiply whatever user input is in the input
         nominal.text = str(int(nominal.text) * change_value)
+        #TODO Trying to work on getting this to work with decimals, so I could do 1.5x, but guess I could just use addition..?
+        if nominal == float:
+            nominal.text = round(nominal.text)
+            nominal.text = (str(int(nominal.text)))
     print(MSG)
 
 
 #Nominal modifiers
 #Changing nominal values by adding user value to pre-existing value
 def nominal_addition_modifier(change_value):
+    """Addition modifier for nominal values, takes parameter change_value as a user input value and loops through all
+    parts in the root xml to change values."""
     #For loop to loop through the <nominal></nominal> tag
     for nominal in root.iter("nominal"):
         #Add whatever integer the user puts in to the pre-existing value.
         nominal.text = str(int(nominal.text) + change_value)
+
     print(MSG)
 
 
@@ -61,18 +77,26 @@ def nominal_addition_modifier(change_value):
 
 #function to modify min values
 def minimum_selection():
-    multiply_or_add = int(input("Multiply or Add Min?(0 - Multiply, 1 - Add): "))
+    """Min selection menu for editing min values in xml."""
+    multiply_or_add = int(input("\033[0;35;48mMultiply or Add Min? (0 = Multiply, 1 = Add):\n"))
     if multiply_or_add == 0:
-        change_value = int(input("How much to multiply each min value by? Use integers:"))
+        change_value = float(input("\033[0;36;48mHow much to multiply each min value by? Use integers:\n"))
         #minimum_multiplication_modifier(change_value)
+        #Check if you're doing too much..
+        if change_value >= 6:
+            print("\033[0;31;48m Integer is too large. Please try a smaller number.")
+            #If you are, try again.
+            minimum_selection()
     elif multiply_or_add == 1:
-        change_value = int(input("How much to add to each min value? Use integers: "))
+        change_value = float(input("\033[0;36;48mHow much to add to each min value? Use integers:\n"))
         minimum_addition_modifier(change_value)
 
 
 #Modifier to add to mins
 #If min value is 2 and you add 4 the new min will be 6.
 def minimum_addition_modifier(change_value):
+    """Addition modifier for min values, takes parameter change_value as a user input and loops through all
+    parts in the root xml to change values."""
     #Loop through all min elements
     for min in root.iter("min"):
         #access values in min and add user input value
@@ -83,6 +107,8 @@ def minimum_addition_modifier(change_value):
 #Modifier to multiply from min
 #If min value is 2 and you multiply by 2 the new min will be 4.
 def minimum_multiplication_modifier(change_value):
+    """Multiplication modifier for min values, takes parameter change_value as a user input and loops through
+    all parts in the root xml to change values."""
     #loop through all min elements
     for min in root.iter("min"):
         #access values in min and multiply by user input value
@@ -94,9 +120,11 @@ def minimum_multiplication_modifier(change_value):
 #-------- Quantmin Modifier --------#
 #Commentating my issues with this function:
 #Python is always reading the xml as a string, so always convert our inputs or values to a str() first.
+#TODO Add the ability to put 0 up to 100, nothing less or higher.
 def quantmin_modifier():
-    print("Must use integer and entire value, so if you want 80% type 80.")
-    change_value = int(input("What would you like to set the quantmin to?: "))
+    """Quantmin modifier, modifies from 0 to 100. Does not modify values set at -1."""
+    print("\033[0;35;48mMust use integer and entire value, so if you want 80% type 80.")
+    change_value = int(input("\033[0;36;48mWhat would you like to set the quantmin to?:\n"))
     for quantmin in root.iter("quantmin"):
         quantmin.text = str(int(quantmin.text))
         if quantmin.text != "-1": #The -1 in the xml is a int, but Python is reading as a string, so had to change this to "-1"
@@ -110,10 +138,11 @@ def quantmin_modifier():
 
 #-------- Quantmax Modifier ---------#
 def quantmax_modifier():
+    """Quantmax modifier, modifies from 0 to 100. Does not modify values set at -1."""
     #Little bit of direction for users
-    print("Must use integer and entire value, so if you want 100% type 100.")
+    print("\033[0;35;48mMust use integer and entire value, so if you want 100% type 100.")
     #Get input
-    change_value = int(input("What would you like to set quantmax to?: "))
+    change_value = int(input("\033[0;36;48mWhat would you like to set quantmax to?:\n"))
     #run loop
     for quantmax in root.iter("quantmax"):
         quantmax.text = str(int(quantmax.text))
@@ -122,6 +151,8 @@ def quantmax_modifier():
             quantmax.text = str(change_value)
         elif quantmax.text == "-1":
             quantmax.text = "-1"
+        else:
+            print("\033[0;31;48mError. Please re-try..") #Red
     print(MSG)
 
 
@@ -155,15 +186,14 @@ menu_stop = False
 #TODO Finish writing options menu for specific selections.
 while menu_stop == False:
     #Used a variable instead of a print statement to help cleanliness
-    options_menu = """
+    options_menu = """\033[0;36;48m
     DZXML Types Modifier
-    Please read the documentation before using. Thank you!
+    Please read the documentation before using. Thank you!\n
     1) Modify Nominal Values
     2) Modify Minimum Values
     3) Modify Quantity Minimum Values
     4) Modify Quantity Maximum Values
-    0) Exit and write to new file
-    """
+    0) Exit and write to new file"""
     #Print out the options menu now
     print(options_menu)
     #Now get the input from the user on which option to utilize
